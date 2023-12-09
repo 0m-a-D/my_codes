2b95727b5881118cc9dd9c967a0766138528cf1a
Om Anand <omishere21@gmail.com>
2023-12-10 01:02:47 +0530

    refactor code with lib.rs

--- matrix_mult/src/main.rs
+++ matrix_mult/src/main.rs
@@ -1,81 +1,4 @@
-use std::io;
-struct Matrix {
-    row: usize,
-    col: usize,
-    elements: Vec<Vec<f32>>, /* need more understanding */
-}
-impl Matrix {
-    fn input_dim(&mut self) {
-        println!("enter number of rows: ");
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("failed to read line");
-        self.row = input.trim().parse().expect("failed to parse input");
-
-        println!("enter number of columns: ");
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("failed to read line");
-        self.col = input.trim().parse().expect("failed to parse input");
-
-        self.elements = vec![vec![0.0; self.col]; self.row];
-    }
-    fn input_dim_valid(&self, other: &Matrix) {
-        if self.col != other.row {
-            panic!("Invalid dimensions for matrix multiplication");
-        } else {
-            println!("Matrix dimensions verified for multiplication. Continue!")
-        }
-    }
-    fn input_elem(&mut self) {
-        for i in 0..self.row {
-            for j in 0..self.col {
-                println!("Enter element at position ({}, {}):", i + 1, j + 1);
-                let mut input = String::new();
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("failed to read line");
-                let element: f32 = input.trim().parse().expect("failed to parse input");
-                self.elements[i][j] = element;
-            }
-        }
-    }
-    fn matrix_mult(&self, other: &Matrix) -> Matrix {
-        let mut result = Matrix {
-            row: self.row,
-            col: other.col,
-            elements: vec![vec![0.0; other.col]; self.row],
-        };
-
-        for i in 0..self.row {
-            for j in 0..other.col {
-                for k in 0..self.col {
-                    result.elements[i][j] += self.elements[i][k] * other.elements[k][j];
-                }
-            }
-        }
-
-        result
-    }
-    fn mult_result(&self) {
-        for i in 0..self.row {
-            for j in 0..self.col {
-                print!("{}\t ", self.elements[i][j]);
-            }
-            println!();
-        }
-    }
-    fn matrix_trans(&self) {
-        for i in 0..self.col {
-            for j in 0..self.row {
-                print!("{}\t ", self.elements[j][i]);
-            }
-            println!();
-        }
-    }
-}
+use matrix_mult::Matrix;
 fn main() {
     println!("<dimensions of first matrix>");
     let mut mat1 = Matrix {
@@ -100,5 +23,10 @@ fn main() {
     println!("result after matrix multiplication is: ");
     result.mult_result();
     println!("transpose of resultant matrix is: ");
-    result.matrix_trans();
+    result.matrix_transpose();
+
+    //define user defined inputs for number for matrices and perform
+    //operations on top of them...
 }
+
+//build another function for finding inverse of the matrix...
