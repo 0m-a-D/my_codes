9afbedd3b7ea581a8269f95027579f150ba28883
Om Anand <omishere21@gmail.com>
2024-01-11 02:00:40 +0530

    add functionality of ignore_case
    write messages to standard error instead of standard output

--- minigrep_io/src/lib.rs
+++ minigrep_io/src/lib.rs
@@ -1,7 +1,8 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 pub struct Config<'a> {
     pub query: &'a String,
     pub file_path: &'a String,
+    pub ignore_case: bool,
 }
 impl<'a> Config<'a> {
     pub fn build(args: &[String]) -> Result<Config, &str> {
@@ -10,7 +11,12 @@ impl<'a> Config<'a> {
         }
         let query = &args[1];
         let file_path = &args[2];
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 #[allow(unused)]
@@ -18,7 +24,12 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // "dyn" is keyword that's short for "dynamic"
     let contents = fs::read_to_string(config.file_path)?;
     // println!("with text:\n{}", contents);
-    for line in search(config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in results {
         println!("{}", line);
     }
     Ok(())
@@ -36,17 +47,44 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- minigrep_io/src/main.rs
+++ minigrep_io/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args); -> commenting this line as args moved into the dbg! macro
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments! {}", err);
+        eprintln!("problem parsing arguments! {}", err);
         process::exit(1);
     });
 
@@ -12,7 +12,7 @@ fn main() {
     println!("in file '{}'", config.file_path);
 
     if let Err(e) = minigrep_io::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
