cc2e52701951fff9a8441cda9867fa4600aa4792
Om Anand <omishere21@gmail.com>
2024-03-16 20:56:16 +0530

    Send Image Bytes to Client

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,6 +1,7 @@
+use image::ImageFormat;
 use std::{
     fs,
-    io::{prelude::*, BufReader},
+    io::{prelude::*, BufReader, Cursor, Write},
     net::{TcpListener, TcpStream},
     thread,
     time::Duration,
@@ -9,14 +10,7 @@ use web_server::ThreadPool;
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    /*
-    let http_request: &Vec<_> = &buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("{:?}", http_request);
-    */
+
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     let (status_line, filename) = match &request_line[..] {
@@ -27,18 +21,31 @@ fn handle_connection(mut stream: TcpStream) {
         }
         "GET /newpage HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /muamua HTTP/1.1" => ("HTTP/1.1 200 OK", "muamua.html"),
+        "GET /muamua.png HTTP/1.1" => ("HTTP/1.1 200 OK", "muamua.png"),
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
-    let contents = fs::read_to_string(filename).unwrap();
+    // let contents = fs::read_to_string(filename).unwrap();
+    let contents = if filename.ends_with(".png") {
+        let image_data = fs::read(filename).unwrap();
+        let image = image::load_from_memory(&image_data).unwrap();
+        let mut image_buffer = Vec::new();
+
+        // use temporary cursor as a seekable buffer
+        let mut cursor = Cursor::new(Vec::new());
+        image.write_to(&mut cursor, ImageFormat::Png).unwrap();
+        image_buffer.extend_from_slice(&cursor.into_inner());
+
+        image_buffer
+    } else {
+        fs::read(filename).unwrap()
+    };
     let length = contents.len();
 
-    let response = format!(
-        "{}\r\nContent-length: {}\r\n\r\n{}",
-        status_line, length, contents
-    );
+    let response = format!("{}\r\nContent-length: {}\r\n\r\n", status_line, length);
     stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(&contents).unwrap();
 }
-#[allow(unused)]
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(20);
